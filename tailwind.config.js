a8a149d70c929833e00dcaa511861d9f4d66ca8a
Monica <monica.jhan@gmail.com>
2022-08-30 17:00:08 -0400

    designed value of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './src/input.css',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00bae0',
+        hover: '#3993B0',
+        dark: '#2f2f2f',
+        darkgray: '#333333',
+        lightgray: '#f9f9f9',
+        lightblue: '#EDFCFF',
+        footer: '#6c6c6c',
+      },
+      fontFamily: {
+        sans: ['Barlow', 'sans-serif'],
+        heading:['Barlow', 'sans-serif'],
+        mackinack: ['p22-mackinac-pro', 'serif'],
+        condensed: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
